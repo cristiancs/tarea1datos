5e9a4257ab9881f07453b4b589e7fb9cb172fb58
CristiaN <cristiannavarrete@outlook.com>
2016-04-22 19:17:35 -0300

    wtf

--- pCML.c
+++ pCML.c
@@ -14,20 +14,25 @@ int pCML (char **S, int n){
 	int size;
 	qsort(S,n,sizeof(char*), cmpfunc);
 
-	int *cantidadPalabras = calloc(27, sizeof(int));
-	char *anterior = malloc(sizeof(char));
+	int *cantidadPalabras = (int *)calloc(27, sizeof(int));
+	char *anterior = (char *) malloc(sizeof(char));
 	char posletra = -1;
-	char *primeraletra = malloc(sizeof(char));
+	char *primeraletra = (char *) malloc(sizeof(char));
 	*anterior = 'z';
+
+	for(i=0; i < n; i++){
+		printf("%d %s \n",i, S[i]);
+	}
 
 
 	// Contar cuantas tenemos que empiecen con cada letra del abecedario
-	while(i <= n){
+	for(i = 0;i <= n; i++){
 		*primeraletra = *S[i];
 		if( strcmp(primeraletra,anterior) != 0){
 			// printf("Letras diferentes %s %s \n",anterior,primeraletra);
 			*anterior = *S[i];
 			posletra = (int)*primeraletra-97;
+			//printf("%d",posletra);
 			cantidadPalabras[posletra]++;
 		}
 		else{
@@ -35,7 +40,6 @@ int pCML (char **S, int n){
 			cantidadPalabras[posletra]++;
 			// printf("Letras iguales, sumamos %d \n",cantidadPalabras[posletra]);
 		}
-		i++;
 	}
 	
 	free((void *)primeraletra);
@@ -43,83 +47,92 @@ int pCML (char **S, int n){
 
 
 
-	for(i=0; i < 27; i++){
-		printf("%d ",cantidadPalabras[i]);
-	}
-
-
+	
 
 	i = 0;
 	int acumulado = 0;
-	int *nPrefijo = calloc(27, sizeof(int)); 
-	int j;
+	int *nPrefijo = (int *) calloc(27, sizeof(int)); 
+	int *nPrefijoTemp = (int *) calloc(27, sizeof(int)); 
+
+
+	int j = 0;
 	int algunsimilar = 0;
 	int indice = 0; // Letra ordenada en la que voy
-	while(i <= n){
+	int i2 = 0;
+	for(i=0; i < n;++i){
+		printf("--HOLA-- %d\n", i);
 		// Revisar que siga siendo la misma letra
 		// S[i] -> palabra
+		printf("%c\n", *S[i]);
 		if((int)*S[i]-97 != indice){
 			indice = (int)*S[i]-97;
 		}
-		printf("%d\n", indice);
-		if(i >= acumulado){
-			nPrefijo[indice]+=1;
-			printf(" Palabra es la que inicia con la letra %d\n",indice);
-			printf(" Palabras que tienen la letra %d es %d\n",indice,cantidadPalabras[indice]);
-			for(j = 1; j <= 200; j++){
-				algunsimilar = 0;
-				// Verificar que tenga la letra
-				// Si es el último
-				
-				if(i-1 == n){
-					printf("%d es el último\n", j);
-					if(S[i-1][j] != S[i][j]){
-						printf("%d es diferente a el anterior \n", j);
-						S[i] = "0\0";
+		printf("Iterando %d indice %d\n", i, indice);
+		// if(i >= acumulado){
+		nPrefijoTemp[indice]+=1;
+		// printf(" Palabra es la que inicia con la letra %d\n",indice);
+		// printf(" Palabras que tienen la letra %d es %d\n",indice,cantidadPalabras[indice]);
+		for(j = 1; j <= 200; j++){
+			algunsimilar = 0;
+			// Verificar que tenga la letra
+			// Si es el último
+
+			if(i == n){
+				// printf("%d es el último\n", j);
+				if(S[i-1][j] != S[i][j]){
+					printf("%d es diferente a el anterior \n", j);
+					S[i] = 0;
+				}
+				else{
+					// printf("%d es igual a el anterior \n", j);
+					algunsimilar = 1;
+				}
+			}
+
+			else if(strlen(S[i]) > j && strlen(S[i+1]) > j){
+				// printf("  Itero letras %d\n",j);
+				// printf("   La palabra tiene mas letras que %d\n",j);
+				// Si es el primero
+				if(i == 0){
+					// printf("%d es el primero\n", j);
+					if(S[i][j] !=  S[i+1][j]){
+						printf("%d es diferente a el siguiente \n", j);
+						S[i] = 0;
 					}
 					else{
-						printf("%d es igual a el anterior \n", j);
+						// printf("%d es igual a el siguiente \n", j);
 						algunsimilar = 1;
 					}
 				}
+				
 
-				else if(strlen(S[i]) > j && strlen(S[i+1]) > j){
-					printf("  Itero letras %d\n",j);
-					printf("   La palabra tiene mas letras que %d\n",j);
-					// Si es el primero
-					if(i == 0){
-						printf("%d es el primero\n", j);
-						if(S[i][j] !=  S[i+1][j]){
-							printf("%d es diferente a el siguiente \n", j);
-							S[i] = "0\0";
-						}
-						else{
-							printf("%d es igual a el siguiente \n", j);
-							algunsimilar = 1;
-						}
+				// Los demas
+				else if( (S[i][j] !=  S[i+1][j]) && (S[i-1][j] != S[i][j]) ){
+					// printf("%d esta entre medio y es diferente\n", j);
+					S[i] = 0;
+				}
+				else{
+					// printf("%d esta entre medio y es similar\n", j);
+					algunsimilar = 1;
+				}
+				if(algunsimilar == 1){
+					// printf("la letra %d tiene similar en la posicion %d\n",indice, j);
+					nPrefijoTemp[indice]+=1;
+					if(nPrefijoTemp[indice] >= nPrefijo[indice]){
+						nPrefijo[indice] = nPrefijoTemp[indice];
 					}
 					
-
-					// Los demas
-					else if( (S[i][j] !=  S[i+1][j]) && (S[i-1][j] != S[i][j]) ){
-						printf("%d esta entre medio y es diferente\n", j);
-						S[i] = "0\0";
-					}
-					else{
-						printf("%d esta entre medio y es similar\n", j);
-						algunsimilar = 1;
-					}
-					if(algunsimilar == 1){
-						printf("la letra %d tiene similar en la posicion %d\n",indice, j);
-						nPrefijo[indice]+=1;
-						printf("El acumulado de %d va en %d \n",indice, nPrefijo[indice]);
-					}
+					// printf("El acumulado de %d va en %d \n",indice, nPrefijo[indice]);
 				}
-				
 			}
-			acumulado+= cantidadPalabras[indice];
 		}
-		i+=1;
+		nPrefijoTemp[indice] = 0;
+		acumulado+= cantidadPalabras[indice];
+		for (i2 = 0; i2 < 27; ++i2){
+			printf("%d ", nPrefijo[i2]);
+		}
+		printf("\n");
+		// }
 	}
 
 
@@ -131,6 +144,6 @@ int pCML (char **S, int n){
 			max = nPrefijo[i];
 		}
 	}
-	printf("\n %d \n", max);
+	// printf("\n %d \n", max);
 	return max;
 }
